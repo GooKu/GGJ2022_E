b1b8b9bcad88734b57a25cdafd3034ea2ffeb86d
jane901201 <jane36742715@gmail.com>
2022-01-22 14:28:42 +0800

    CountdownSystem complete

--- Assets/Scripts/Other/CountdownSystem.cs
+++ Assets/Scripts/Other/CountdownSystem.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 
 public class CountdownSystem : MonoBehaviour
 {
-    [SerializeField] private int m_TotalTime;
-    private int m_CurrentTime;
+    [SerializeField] private float m_TotalTime;
+    private float m_CurrentTime;
+
+    public float CurrentTime { get => m_CurrentTime; set => m_CurrentTime = value; }
 
     private void Start()
     {
         m_CurrentTime = m_TotalTime;
+        StartCoroutine(StartCountdown());
+    }
+
+    private void FixedUpdate()
+    {
+        m_CurrentTime -= Time.fixedDeltaTime;
     }
 
     public IEnumerator StartCountdown()
     {
-        yield return new WaitUntil(() => m_CurrentTime == 0);
+        yield return new WaitUntil(() => m_CurrentTime <= 0);
+        Debug.Log("Wait complete");
     }
 }
--- Assets/Scripts/Other/CountdownUI.cs
+++ Assets/Scripts/Other/CountdownUI.cs
@@ -7,15 +7,9 @@ public class CountdownUI : MonoBehaviour
     [SerializeField] private Text m_Text;
     [SerializeField] private CountdownSystem m_CountdownSystem;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-
+        m_Text.text = "倒數:" + ((int)m_CountdownSystem.CurrentTime);
     }
 }
