79c6a4d10f2297abb0a3f8eef091e095ce0dacea
kerrytsou <karry9527100@gmail.com>
2022-01-23 12:42:05 +0800

    Add TextController

--- Assets/Scripts/Other/StoryGameOverSetting.cs
+++ Assets/Scripts/Other/StoryGameOverSetting.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,11 +14,14 @@ public class StoryGameOverSetting : MonoBehaviour
 
     // Bad End
     [SerializeField] private GameObject m_BadEndObject;
+    [SerializeField] private GameObject m_BadEndBG;
 
     // True End
     [SerializeField] private GameObject m_TrueEndObject;
+    [SerializeField] private GameObject m_TrueEndBG;
 
     // End game popup
+    [SerializeField] private GameObject m_Popup;
     [SerializeField] private Image m_EndTitle;
     [SerializeField] private Sprite m_HappyEndSprite;
     [SerializeField] private Sprite m_BadEndSprite;
@@ -26,10 +30,12 @@ public class StoryGameOverSetting : MonoBehaviour
     void Start()
     {
         SetStoryEvent();
+        TextController.Instance.TextEndEvent += StoryEnd;
     }
 
     public void SetStoryEvent()
     {
+        m_Popup.SetActive(false);
         if (m_StoryEnd.StoryEndType == StoryEndType.HappyEnd)
         {
             //m_EndText.text = "快樂結局";
@@ -37,22 +43,44 @@ public class StoryGameOverSetting : MonoBehaviour
             m_BadEndObject.SetActive(false);
             m_TrueEndObject.SetActive(false);
             m_EndTitle.sprite = m_HappyEndSprite;
+
+            TextController.Instance.HappyEndText(m_EndText);
         }
         else if (m_StoryEnd.StoryEndType == StoryEndType.BadEnd)
         {
+            m_BadEndBG.SetActive(false);
+
             //m_EndText.text = "壞結局";
             m_HappyEndObject.SetActive(false);
             m_BadEndObject.SetActive(true);
             m_TrueEndObject.SetActive(false);
             m_EndTitle.sprite = m_BadEndSprite;
+
+            TextController.Instance.ChangeSceneEvent += () => m_BadEndBG.SetActive(true);
+            TextController.Instance.BadEndText(m_EndText);
         }
         else if (m_StoryEnd.StoryEndType == StoryEndType.TrueEnd)
         {
+            m_TrueEndBG.SetActive(false);
+
             //m_EndText.text = "真結局";
             m_HappyEndObject.SetActive(false);
             m_BadEndObject.SetActive(false);
             m_TrueEndObject.SetActive(true);
             m_EndTitle.sprite = m_TrueEndSprite;
+
+            TextController.Instance.ChangeSceneEvent += () => m_TrueEndBG.SetActive(true);
+            TextController.Instance.TrueEndText(m_EndText);
         }
+    }
+
+    void StoryEnd()
+    {
+        m_Popup.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        TextController.Instance.TextEndEvent -= StoryEnd;
     }
 }
--- Assets/Scripts/Other/StorySetting.cs
+++ Assets/Scripts/Other/StorySetting.cs
@@ -1,12 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StorySetting : MonoBehaviour
 {
+    [SerializeField] private Text text;
+    [SerializeField] private SceneController sceneController;
+
     // Start is called before the first frame update
     void Start()
     {
         MusicController.Instance.ChangeBGM(MusicController.AudioType.HappyEndBGM);
+        TextController.Instance.StoryStartText(text);
+        TextController.Instance.TextEndEvent += StoryEnd;
+    }
+
+    public void StoryEnd()
+    {
+        sceneController.SwitchScene("Game");
+    }
+
+    private void OnDestroy()
+    {
+        TextController.Instance.TextEndEvent -= StoryEnd;
     }
 }
